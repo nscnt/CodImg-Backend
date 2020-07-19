b360fdc856d69a3e66cf5106e7950e886a149b97
EliteDaMyth <elitemythyt@gmail.com>
2020-07-19 13:58:31 +0530

    more prettify

--- api/languages.js
+++ api/languages.js
@@ -1,10 +1,21 @@
-const languages = ["c","css","cpp","go","html","java","javascript","python","rust","typescript"];
+const languages = [
+  "c",
+  "css",
+  "cpp",
+  "go",
+  "html",
+  "java",
+  "javascript",
+  "python",
+  "rust",
+  "typescript",
+];
 
 module.exports = (request, response) => {
-    console.log('');
-    console.log('🎉 ', request.url);
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.json({
-        languages
-    });
-}
+  console.log("");
+  console.log("🎉 ", request.url);
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.json({
+    languages,
+  });
+};
